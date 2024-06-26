b972e163bf3bd38a56a84ceac467888de85b5a61
joylan21 <joylandsouza21@gmail.com>
2024-06-26 23:57:50 +0530

    dsds

--- core/filters.py
+++ core/filters.py
@@ -5,7 +5,7 @@ class ArtsFilter(filters.FilterSet):
     category = filters.CharFilter(field_name='category__name', lookup_expr='icontains')
     title = filters.CharFilter(field_name='title', lookup_expr='icontains')
     author_id = filters.NumberFilter(field_name='author__id', lookup_expr='iexact')
-    author_name = filters.NumberFilter(field_name='author__name', lookup_expr='icontains')
+    author_name = filters.CharFilter(field_name='author__name', lookup_expr='icontains')
 
     class Meta:
         model = Art
